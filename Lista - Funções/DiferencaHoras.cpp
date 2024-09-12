6f8ea2a3998067bab43ae31e546bf3526eab318b
Bowie <anacarolinesousadoamaral2020@gmail.com>
2024-09-12 14:58:32 -0300

    diferenca segundos

--- Lista - Funções/DiferencaHoras.cpp
+++ Lista - Funções/DiferencaHoras.cpp
@@ -3,27 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void LeVetor(int a, int b, int c, int d, int e, int f)
+void LeHoras(int &a, int &b, int &c, int &d, int &e, int &f)
 {
-    printf("Digite a hora inicial:\n");
-    scanf("%d%d%d", &a, &b, &c);
-    printf("Digite a hora final:\n");
-    scanf("%d%d%d", &d, &e, &f);
+    printf("Digite a hora inicial (hh : mm : ss):\n");
+    scanf("%d %d %d", &a, &b, &c);
+    printf("Digite a hora final (hh : mm : ss):\n");
+    scanf("%d %d %d", &d, &e, &f);
 }
 
 int ConverteSegundos(int horaI, int minI, int segI, int horaF, int minF, int segF)
 {
     int SomaSegundosI, SomaSegundosF, Diferenca;
-    horaI = horaI * 3600;
-    minI = minI * 60;
-    segI = segI;
-    horaF = horaF * 3600;
-    minF = minF * 60;
-    segF = segF;
-
-    SomaSegundosI = horaI + minI + segI;
-    SomaSegundosF = horaF + minF + segF;
-
+    
+    SomaSegundosI = horaI * 3600 + minI * 60 + segI;
+    SomaSegundosF = horaF * 3600 + minF * 60 + segF;
+    
     Diferenca = SomaSegundosF - SomaSegundosI;
 
     return Diferenca;
@@ -31,13 +25,17 @@ int ConverteSegundos(int horaI, int minI, int segI, int horaF, int minF, int seg
 
 void ExibeDiferenca(int Diferenca)
 {
-    printf("A diferenca em segundos da hora final para a inicial é de %d segundos\n", Diferenca);
+    printf("A diferença em segundos da hora final para a inicial eh de %d segundos.\n", Diferenca);
     getch();
 }
 
 int main(void)
 {
-    int horaI, minI, segI, horaF, minF, segF;
-
-    LeVetor(&horaI, &minI, &segI, &horaF, &minF, &segF);
+    int hi, mi, si, hf, mf, sf, diferenca;
+    
+    LeHoras(hi, mi, si, hf, mf, sf);
+    diferenca = ConverteSegundos(hi, mi, si, hf, mf, sf);
+    ExibeDiferenca(diferenca);
+    
+    return 0;
 }
