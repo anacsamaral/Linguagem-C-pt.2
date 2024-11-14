6d4f4a1915aedbea0cfb714d4207a29e0bba8030
Bowie <anacarolinesousadoamaral2020@gmail.com>
2024-11-13 21:06:08 -0300

    update

--- Lista - Arquivo/Arquivo2.cpp
+++ Lista - Arquivo/Arquivo2.cpp
@@ -8,8 +8,7 @@
 struct Aluno //int tem 4 bytes, string 44 bytes = 48 bytes
 {
 	char RA[14], Nome[30]; //cada char tem 1 byte
-	int AnoNasc;
-	char flag; 
+	int AnoNasc, Status;
 };
 
 char Menu(void)
@@ -21,8 +20,7 @@ char Menu(void)
 	printf("\n[C] Consultar");
 	printf("\n[D] Alterar por R.A.");
 	printf("\n[E] Ordenar por R.A.");
-	printf("\n[F] Excluir por R.A.(Fisica)");
-	printf("\n[G] Excluir por R.A.(Logica)");
+	printf("\n[F] Excluir Fisicamente por R.A.");
 	printf("\n[ESC] Finalizar");
 	printf("\nOpcao Desejada: ");
 	return toupper(getche());
@@ -33,12 +31,26 @@ int BuscaAlunoRA(FILE *PtrAluno, char RA[14]) //arquivo passa aberto
 	Aluno Reg;
 	rewind(PtrAluno); //Posiciona o PtrAluno no INï¿½CIO DO ARQUIVO;
 	fread(&Reg, sizeof(Aluno), 1, PtrAluno);
-	while(!feof(PtrAluno) && strcmp(RA, Reg.RA)!=0)
-		fread(&Reg, sizeof(Aluno), 1, PtrAluno);
+	//while(!feof(PtrAluno) && !(strcmp(ChaveRA, Reg.RA) == 0 && R.Status))
+	//while(!feof(PtrAluno) && (strcmp(ChaveRA, Reg.RA) != 0 && R.Status != 0))
+	//enquanto nao for o final do arquivo e RA diferente de zero ou Status diferente de zero
+	//só para quando os dois de OR forem verdadeiros
+	//fununciona
+	int achou = 0;
+	while(!feof(PtrAluo) && !achou)
+	{
+		if(strcmp(ChaveRA, Reg.RA) == 0 && R.Status)
+			achou = 1;
+		else
+			fread(&R, sizeof(Aluno), 1, PtrAluno);
+	}
+	fread(&Reg, sizeof(Aluno), 1, PtrAluno);
+		
 	
-	if(strcmp(RA, Reg.RA)==0)
+	//if(strcmp(RA, Reg.RA)==0 && R.Status)
+	if(achou)
 		return ftell(PtrAluno)-sizeof(Aluno); 
-		//tell diz onde esta o deslocamentoponteiro;
+		//tell diz onde esta o deslocamento ponteiro;
 		//onde esta o ponteiro, menos uma estrutura;
 	else
 		return -1;
@@ -48,7 +60,7 @@ void OrdenarAlunos(void)
 {
 	int a, b, QtdeReg;
 	Aluno RegA, RegB;
-	FILE *PtrAlu = fopen("Alunos.dat", "rb+");
+	FILE *PtrAlu = fopen("AlunosLOG.dat", "rb+");
 	if(PtrAlu == NULL)
 		printf("\nErro de Abertura\n");
 	else
@@ -91,7 +103,7 @@ void GravarAluno(void)
 	Aluno Reg;
 	printf("%d", sizeof(Aluno)); //qtd de bytes q tem na struct
 	//Ponteiro sï¿½ vai existir se a funï¿½ï¿½o for chamada;
-	FILE *Ptr = fopen("Alunos.dat", "ab+"); //Sempre serï¿½ esse nome
+	FILE *Ptr = fopen("AlunosLOG.dat", "ab+"); //Sempre serï¿½ esse nome
 	//ab+ para deixar ler, da para usar a busca dentro;
 	//ï¿½ recomendado abrir com rb ou rb+
 	printf("\n## Cadastro de Alunos ##\n");
@@ -106,7 +118,8 @@ void GravarAluno(void)
 			printf("\nAno de Nascimento: ");
 			scanf("%d", &Reg.AnoNasc);
 			
-			Reg.flag = '1';
+			
+			Reg.flag = 1;
 			fwrite(&Reg, sizeof(Aluno),  1, Ptr); //da memoria pro arquivo
 			//O arquivo comeï¿½a nesse endereï¿½o;
 			//Da para usar o nome da variavel ou o tipo dela (recomendado);
@@ -131,7 +144,7 @@ void GravarAluno(void)
 void ExibirAluno(void)
 {
 	Aluno R;
-	FILE *PtrAlu = fopen("Alunos.dat", "rb"); //ponteiro esta no inicio;
+	FILE *PtrAlu = fopen("AlunosLOG.dat", "rb"); //ponteiro esta no inicio;
 	//para abrir com rb, tem que ja existir um arquivo;
 	
 	//VALIDANDO SE O ARQUIVO EXISTE (nï¿½o tem comando, mas)
@@ -145,10 +158,12 @@ void ExibirAluno(void)
 		//esta testando onde executou o fread
 		{
 			//onde esta parado o ponteiro, pega o sizeof e joga para o &R;
-			printf("\nRA: %s", R.RA);
-			printf("\nNome: %s", R.Nome);
-			printf("\nAno Nasc.: %d\n", R.AnoNasc);
-			
+			if(R.Status)
+			{
+				printf("\nRA: %s", R.RA);
+				printf("\nNome: %s", R.Nome);
+				printf("\nAno Nasc.: %d\n", R.AnoNasc);	
+			}
 			fread(&R, sizeof(Aluno), 1, PtrAlu);
 		}
 		//se nao foi aberto, nao pode ser fechado, sï¿½ da fclose se foi aberto
@@ -161,7 +176,7 @@ void ExibirAluno(void)
 void ConsultarAluno(void)
 {
 	int pos;
-	FILE *PtrAlu = fopen("Alunos.dat", "rb");
+	FILE *PtrAlu = fopen("AlunosLOG.dat", "rb");
 	//O arquivo precisa existir;
 	Aluno RegAluno; //Funciona como auxiliar
 	system("cls");
@@ -213,7 +228,7 @@ void ConsultarAluno(void)
 
 void AlterarAluno(void)
 {
-	FILE *PtrAluno = fopen("Alunos.dat", "rb+");//arquivo tem q existir
+	FILE *PtrAluno = fopen("AlunosLOG.dat", "rb+");//arquivo tem q existir
 	Aluno RegAluno;
 	int pos;
 	system("cls");
@@ -271,22 +286,22 @@ void AlterarAluno(void)
 	}
 }
 
-// === EXCLUSAO ===
+// === EXCLUSï¿½O ===
 /*	Criar um arquivo novo que receberï¿½ as informaï¿½ï¿½es do antigo arquivo
-	Deletar o antigo arquivo e renomear o atual -> remove("Alunos.dat")
-	Renomear o arquivo atual -> rename("Temp.dat", "Alunos.dat"))
+	Deletar o antigo arquivo e renomear o atual -> remove("AlunosLOG.dat")
+	Renomear o arquivo atual -> rename("Temp.dat", "AlunosLOG.dat"))
 	
 	
 	 
 */
-	
-void ExclusaoFisica()
+
+void ExclusaFisicaTodos(void)
 {
 	Aluno RegAlu;
 	char AuxRA[14];
 	int pos;
 	
-	FILE *PtrAlu = fopen("Alunos.dat", "rb");
+	FILE *PtrAlu = fopen("AlunosLOG.dat", "rb");
 	system("cls");
 	printf("\n### Exclusao Fisica de um Aluno ###\n");
 	if(PtrAlu ==  NULL)
@@ -331,12 +346,139 @@ void ExclusaoFisica()
 				}
 				fclose(PtrAlu);
 				fclose(PtrTemp);
-				remove("Alunos.dat");
-				rename("Temp.dat", "Alunos.dat");
+				remove("AlunosLOG.dat");
+				rename("Temp.dat", "AlunosLOG.dat");
 				printf("\nAluno Excluido Fisicamente!\n");
 			}
 			else
 				fclose(PtrAlu);
+		}
+	}
+}
+	
+void ExclusaoFisica(void)
+{
+	Aluno RegAlu;
+	char AuxRA[14];
+	int pos;
+	
+	FILE *PtrAlu = fopen("AlunosLOG.dat", "rb");
+	system("cls");
+	printf("\n### Exclusao Fisica de um Aluno ###\n");
+	if(PtrAlu ==  NULL)
+		printf("\nErro de Abertura\n");
+	else
+	{
+		printf("\nR.A. a Excluir: ");
+		fflush(stdin);
+		gets(RegAlu.RA);
+		pos = BuscaAlunoRA(PtrAlu, AuxRA);
+		
+		if(pos == -1)
+		{
+			printf("\nAluno nao Cadastrado!\n");
+			fclose(PtrAlu);
+		}
+			
+		else
+		{
+			fseek(PtrAlu, pos,0);
+			fread(&RegAlu, sizeof(Aluno),1,PtrAlu);
+			
+			printf("\nDados do Aluno:\n");
+			printf("R.A.: %s\n", RegAlu.RA);
+			printf("Nome: %s\n", RegAlu.Nome);
+			printf("Ano Nasc.: %d\n", RegAlu.AnoNasc);
+			
+			printf("\nConfirma Exclusao? (S/N): ");
+			if(toupper(getche()) == 'S')
+			{
+				rewind(PtrAlu); //fseek(PtrAlu, 0, 0);
+				FILE *PtrTemp = fopen("Temp.dat", "wb"); //wb abre o arq zerado
+				fread(&RegAlu, sizeof(Aluno),1,PtrAlu);
+				
+				while(!feof(PtrAlu)) //enquanto arq nao acabou
+				{
+					if(strcmp(AuxRA,RegAlu.RA)!=0)
+						//Se diferente, grava
+						fwrite(&RegAlu, sizeof(Aluno), 1, PtrTemp);
+					
+					fread(&RegAlu, sizeof(Aluno), 1, PtrAlu);
+				}
+				fclose(PtrAlu);
+				fclose(PtrTemp);
+				remove("AlunosLOG.dat");
+				rename("Temp.dat", "AlunosLOG.dat");
+				printf("\nAluno Excluido Fisicamente!\n");
+			}
+			else
+				fclose(PtrAlu);
+		}
+	}
+}
+
+void ExclusaoLogica(void)
+{
+	PtrAluno = fopen("AlunosLOG.dat", "rb+");//arquivo tem q existir
+	Aluno RegAluno;
+	int pos;
+	system("cls");
+	printf("## Alterar Aluno ##");
+	
+	if(PtrAluno == NULL)
+		printf("\nErro de Abertura:(\n");
+	else
+	{
+		printf("\nAluno a ser alterado [R.A.]: ");
+		fflush(stdin);
+		gets(RegAluno.RA);
+		
+		while(strcmp(RegAluno.RA, "\0")!=0)
+		{
+			pos = BuscaAlunoRA(PtrAluno, RegAluno.RA);
+		
+			if (pos == -1)
+				printf("\nAluno nao encontrado!\n");
+			else
+			{
+				fseek(PtrAluno, pos, 0);//bytes a partir do inicio
+				fread(&RegAluno, sizeof(Aluno), 1, PtrAluno);//de onde o ponteiro esta parado e joga pro registro
+				printf("\n*** Dados Encontrados ***");
+				printf("\nR.A: %s", RegAluno.RA);
+				printf("\nNome: %s", RegAluno.Nome);
+				printf("\nAno Nasc: %d\n", RegAluno.AnoNasc);
+				
+				printf("\nDeseja alterar os dados deste RA? [S/N]:\n");
+				if (toupper(getche()) == 'S')
+				{
+					printf("\nNovo Nome: "); fflush(stdin);
+					gets(RegAluno.Nome);
+					printf("Novo Ano de Nascimento: ");
+					scanf("%d", &RegAluno.AnoNasc);
+					
+					fseek(PtrAluno, pos, 0);
+					fwrite(&RegAluno, sizeof(Aluno), 1, PtrAluno);
+					
+					fseek(PtrAluno, pos, 0); //ponteiro vai para a posiï¿½ï¿½o que achou na busca
+					fread(&RegAluno, sizeof(Aluno), 1, PtrAluno); //fread e fwrite ponteiro vai para frente
+					printf("\n*** Dados Alterados ***");
+					printf("\nR.A: %s", RegAluno.RA);
+					printf("\nNome: %s", RegAluno.Nome);
+					printf("\nAno Nasc: %d\n", RegAluno.AnoNasc);
+					printf("\nConfirma Exclusao (S/N)?");
+					if (toupper(getche) == 'S')
+					{
+						RAluno.Status = 0;
+						fseek(PtrAlu,pos,0);
+						fwrite(&RAluno, sizeof(Aluno), 1, PtrAlu);
+						printf("\nRegistro excluido com sucesso!\n");
+					}
+					getch();
+				}
+			}
+			printf("\nR.A. a Excluir [R.A.]: ");
+			fflush(stdin);
+			gets(RegAluno.RA);
 		}
 	}
 }
@@ -375,12 +517,9 @@ int main(void)
 			case 'F':
 					ExclusaoFisica();
 					break;
-			case 'G':
-					ExclusaoLogica();
-					break;
 		}
 		
 	}while(opcao != 27);
-	
+	ExclusaoFisicaTodos();
 	return 0;
 }
